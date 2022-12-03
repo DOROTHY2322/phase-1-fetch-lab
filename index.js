2ea4046e5ba5927a0958b4c195641bb56c218c7f
DOROTHY2322 <kimelidorothy@gmail.com>
2022-12-03 12:20:08 +0300

    FetchLab

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 function fetchBooks() {
  fetch('https://anapioficeandfire.com/api/books')
  .then((response) => response.json())
- .then((json) => renderBooks(json));
-  
-}
+    .then((json) => renderBooks(json));
 
+}
+const get = async () => {
+  try {
+    const res = await fetch('https://anapioficeandfire.com/api/books');
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
+  })
+  
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  
 });
