ed6a1748a691dab9849be8bbb5c918fb5ebbd058
DOROTHY2322 <kimelidorothy@gmail.com>
2022-12-04 18:20:32 +0300

    fetch

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function renderBooks(json) {
   const main = document.querySelector('main');
  json.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = `<h2>${book.name}</h2>`;
+    h2.innerHTML = book.name;
     main.appendChild(h2);
   })
 
